13d87447d91720d5e976f0d78d845db3418ce791
KumikoKazura <egarmin98@mail.ru>
2018-10-10 21:01:48 +0500

    Change method scripting
    Удаление старой версии проекта и добавление новой

---
+++ MainProject/Assets/Scripts/Character.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : Unit
+{
+    [SerializeField]
+    private int lives = 5;
+
+    public int Lives
+    {
+        get { return lives; }
+        set
+        {
+           if (value < 5) lives = value;
+            livesBar.Refresh();
+        }
+    }
+    private LivesBar livesBar;
+
+    [SerializeField]
+    private float playerSpeed = 3.0F;
+    [SerializeField]
+    private float jumpPower = 15.0F;
+
+    private bool groundCheck = false;
+    public int directionInput;
+    public bool facingRight = true;
+
+    private Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        livesBar = FindObjectOfType<LivesBar>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+        rb2d.velocity = new Vector2(playerSpeed * directionInput, rb2d.velocity.y);
+    }
+
+    void Update()
+    {
+        if ((directionInput < 0) && (facingRight))
+        {
+            Flip();
+        }
+
+        if ((directionInput > 0) && (!facingRight))
+        {
+            Flip();
+        }
+        groundCheck = true;
+    }
+
+    public void Run(int dir)
+    {
+        directionInput = dir;
+    }
+
+    public void Jump(bool isJump)
+    {
+        isJump = groundCheck;
+
+        if (groundCheck)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpPower);
+        }
+    }
+
+    public override void ReceiveDamage()
+    {
+        Lives--;
+
+        rb2d.velocity = Vector3.zero;
+        rb2d.AddForce(transform.up * 8.0F, ForceMode2D.Impulse);
+
+        Debug.Log(lives);
+    }
+
+    private void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
+
+        groundCheck = colliders.Length > 1;
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
