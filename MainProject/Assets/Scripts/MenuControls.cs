cfc38f3be87c20ee9eff6180cc44c60232271892
KumikoKazura <egarmin98@mail.ru>
2018-11-01 12:58:32 +0500

    Added main menu and pause

---
+++ MainProject/Assets/Scripts/MenuControls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuControls : MonoBehaviour {
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+}
---
+++ MainProject/Assets/Scripts/Pause.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public float timer;
+    public bool ispuse = false;
+    public bool guipuse;
+    public GameObject panel;
+
+    public void Paus()
+    {
+        ispuse = !ispuse;
+    }
+
+    void Update()
+    {
+        Time.timeScale = timer;
+
+        if (ispuse == true)
+        {
+            timer = 0;
+            guipuse = true;
+
+        }
+        else if (ispuse == false)
+        {
+            timer = 1f;
+            guipuse = false;
+
+        }
+    }
+    public void OnGUI()
+    {
+        if (guipuse == true)
+        {
+            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2) - 150f, 150f, 45f), "Продолжить"))
+            {
+                ispuse = false;
+                timer = 0;
+                panel.SetActive(false);
+            }
+            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2), 150f, 45f), "В Меню"))
+            {
+                ispuse = false;
+                timer = 0;
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
